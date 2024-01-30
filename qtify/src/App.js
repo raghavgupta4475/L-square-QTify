f04f46767f23ed0263d3df32fcbf01fe5ff9123f
raghavgupta4475 <raghavkumargupta18@gmail.com>
2024-01-30 22:53:55 +0530

    Hero section added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
-    <Navbar />
+    <><Navbar /><Hero /></>
   );
 }
 
